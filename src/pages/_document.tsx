57af3613c593a2e4221dc43447b555c2e683b2dd
Morgana <morganabagatini@gmail.com>
2022-04-10 14:47:52 -0300

    dark mode

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
+import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { theme } from "../styles/theme";
 
 export default function Document() {
   return (
@@ -17,6 +19,7 @@ export default function Document() {
         />
       </Head>
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
   colors: {
     blue: {
       900: "#0A111F",
