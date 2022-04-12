0537b9013fa35d987b976ef4b1bcb7098cefd3ba
Morgana <morganabagatini@gmail.com>
2022-04-11 21:17:42 -0300

    Search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
-import { Box, Text, InputGroup, Input, Button, Select } from "@chakra-ui/react";
 import { Search2Icon } from "@chakra-ui/icons";
-import { Select as SelectCustom } from "chakra-react-select";
+import { Button, Flex, Select, Text } from "@chakra-ui/react";
+import { OptionBase, Select as ChakraReactSelect } from "chakra-react-select";
 
 import { useCategories } from "../hooks/useCategories";
 import { useIngredients } from "../hooks/useIngredients";
+import { chakraStyles } from "../styles/selectChakraStyles";
 
 type FilterProps = {
   category: string | null;
   ingredient: string | null;
 };
+
+interface Option extends OptionBase {
+  value: string;
+  label: string;
+}
 
 interface SearchProps {
   handleSearch: (filters: FilterProps) => void;
@@ -17,8 +23,8 @@ interface SearchProps {
 
 export function SearchForm({ handleSearch }: SearchProps) {
   const { data: categories } = useCategories();
-
   const { data: ingredients } = useIngredients();
+
   const optionsIngredients =
     ingredients?.map((item) => {
       return { value: item, label: item };
@@ -27,48 +33,49 @@ export function SearchForm({ handleSearch }: SearchProps) {
   const [category, setCategory] = useState("");
   const [ingredient, setIngredient] = useState("");
 
+  const [ingredientSelect, setIngredientSelect] = useState<Option | null>(null);
+
+  function updateIngredientSelect(item: Option | null) {
+    setIngredientSelect(item);
+    setIngredient(item ? item.value : "");
+  }
+
   return (
-    <Box as="form" w="100%">
-      <InputGroup border="1px">
-        <Select
-          placeholder="Category"
-          w="sm"
-          border="0"
-          borderRadius="0"
-          onChange={(e) => {
-            setCategory(e.target.value);
-            setIngredient("");
-          }}
-          value={category}
-        >
-          {categories &&
-            categories.map((category) => (
-              <option key={category} value={category}>
-                {category}
-              </option>
-            ))}
-        </Select>
+    <Flex as="form" w="100%" border="1px solid white" align="center">
+      <Select
+        placeholder="Category"
+        w="sm"
+        border="0"
+        borderRadius="0"
+        value={category}
+        onChange={(e) => {
+          setCategory(e.target.value);
+          updateIngredientSelect(null);
+        }}
+      >
+        {categories &&
+          categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+      </Select>
 
-        <Text alignSelf="center" mx="4">
-          or
-        </Text>
-      </InputGroup>
+      <Text alignSelf="center" mx="4">
+        or
+      </Text>
 
-      <SelectCustom
+      <ChakraReactSelect
+        placeholder="Select an ingredient"
+        chakraStyles={chakraStyles}
         options={optionsIngredients}
-        placeholder="Select some ingredient"
-        size="lg"
+        value={ingredientSelect}
+        onChange={(option) => {
+          const newValue = option as Option;
+          updateIngredientSelect(newValue);
+          setCategory("");
+        }}
       />
-
-      {/* <Input
-          type="text"
-          placeholder="type an ingedient"
-          border="0"
-          borderRadius="0"
-          value={ingredient}
-          onChange={(e) => setIngredient(e.target.value)}
-          onKeyDown={() => setCategory("")}
-        /> */}
 
       <Button
         type="submit"
@@ -82,6 +89,6 @@ export function SearchForm({ handleSearch }: SearchProps) {
       >
         <Search2Icon color="gray.300" />
       </Button>
-    </Box>
+    </Flex>
   );
 }
