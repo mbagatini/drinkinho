b3ead97b0c4d72c2e86f05744fe8ca18f78b113f
Morgana <morganabagatini@gmail.com>
2022-04-14 17:04:32 -0300

    ts problem

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -13,12 +13,13 @@ import { chakraStyles } from "../styles/selectChakraStyles";
 
 export const CustomSelect = forwardRef(
   <Option, IsMulti extends boolean, Group extends GroupBase<Option>>(
-    props: Props<Option, IsMulti, Group>,
+    // I couldn't solve the type here, so I'm just going to ignore the types
+    props: any, //Props<Option, IsMulti, Group>,
     ref:
       | ((instance: SelectInstance<Option, IsMulti, Group> | null) => void)
       | MutableRefObject<SelectInstance<Option, IsMulti, Group> | null>
       | null
   ) => {
-    return <Select ref={ref} chakraStyles={chakraStyles} {...props} />;
+    return <Select ref={ref} {...props} chakraStyles={chakraStyles} />;
   }
 ) as SelectComponent;
